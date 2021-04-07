ed032652d8287076ba8988f74d730120a656e996
Rychell Sousa <rychell@gmail.com>
2021-04-06 22:50:11 -0300

    User repository created

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { inject } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dto/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({
+    name,
+    username,
+    password,
+    driver_license,
+    email,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      username,
+      password,
+      driver_license,
+      email,
+    });
+  }
+}
+
+export { CreateUserUseCase };
