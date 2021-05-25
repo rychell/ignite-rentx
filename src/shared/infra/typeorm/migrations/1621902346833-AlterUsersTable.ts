abc0fa4896c5de889a7dfcf896a7055e35951a64
Rychell Sousa <rychell@gmail.com>
2021-05-24 22:00:37 -0300

    Create migrations for rentals table

---
+++ src/shared/infra/typeorm/migrations/1621902346833-AlterUsersTable.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUsersTable1621902346833 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      "users",
+      "id",
+      new TableColumn({
+        name: "id",
+        type: "uuid",
+        isPrimary: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      "users",
+      "id",
+      new TableColumn({
+        name: "id",
+        type: "uuid",
+        isPrimary: false,
+      })
+    );
+  }
+}
